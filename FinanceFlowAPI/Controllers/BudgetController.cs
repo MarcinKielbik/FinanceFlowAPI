d9e04bbda6bc205ab5d14508d32cbd8c31ea67ed
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-02-01 19:58:59 +0100

    Implement BudgetController with CRUD operations

--- FinanceFlowAPI/Controllers/BudgetController.cs
+++ FinanceFlowAPI/Controllers/BudgetController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FinanceFlowAPI.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinanceFlowAPI.Controllers
@@ -7,6 +8,62 @@ namespace FinanceFlowAPI.Controllers
     [ApiController]
     public class BudgetController : ControllerBase
     {
+        private readonly IBudgetService _budgetService;
 
+        public BudgetController(IBudgetService budgetService)
+        {
+            _budgetService = budgetService;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetRemainingBudget(int userId)
+        {
+            var remainingBudget = await _budgetService.GetRemainingBudget(userId);
+
+            if (remainingBudget == 0)
+                return NotFound("No budget found for this user.");
+
+            return Ok(remainingBudget);
+        }
+
+        [HttpPost("{userId}")]
+        public async Task<IActionResult> CreateBudget(int userId, [FromBody] decimal amount)
+        {
+            var success = await _budgetService.CreateBudgetAsync(userId, amount);
+
+            if(!success)
+            {
+                return BadRequest("Budget already exists for this user.");
+            }
+
+            return CreatedAtAction(nameof(CreateBudget), userId, amount);
+        }
+
+        [HttpPut("{userId}")]
+        public async Task<IActionResult> UpdateBudget(int userId, [FromBody] decimal amount)
+        {
+            var success = await _budgetService.UpdateBudgetAsync(userId, amount);
+
+            if (!success)
+            {
+                return NotFound("Budget not found for this user.");
+            }
+
+            return NoContent(); // No content, successful update
+        }
+
+
+        [HttpDelete("{userId}")]
+        public async Task<IActionResult> DeleteBudget(int userId)
+        {
+            var success = await _budgetService.DeleteBudgetAsync(userId);
+
+            if (!success)
+            {
+                return NotFound("Budget not found for this user.");
+            }
+
+            return NoContent(); // No content, successful delete
+        }
     }
 }
