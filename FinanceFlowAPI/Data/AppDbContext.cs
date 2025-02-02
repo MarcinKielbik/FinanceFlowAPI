44ecf0686332a9a725bb2d506be68fd63beb5c41
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-02-02 19:47:13 +0100

    Configure Identity with roles(Program.cs), update AppDbContext, and initialize first migration

--- FinanceFlowAPI/Data/AppDbContext.cs
+++ FinanceFlowAPI/Data/AppDbContext.cs
@@ -1,16 +1,31 @@
 ﻿using FinanceFlowAPI.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinanceFlowAPI.Data
 {
-    public class AppDbContext : IdentityDbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
-        // public DbSet<User> Users { get; set; }
         public DbSet<Budget> Budgets { get; set; }
         public DbSet<Expense> Expenses { get; set; }
         public DbSet<Category> Categories { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Wywołanie metody bazowej dla poprawnej konfiguracji Identity
+            base.OnModelCreating(modelBuilder);
+
+            // Ustawienie precyzji dla wartości typu decimal
+            modelBuilder.Entity<Budget>()
+                .Property(b => b.Amount)
+                .HasColumnType("decimal(18, 2)");
+
+            modelBuilder.Entity<Expense>()
+                .Property(e => e.Amount)
+                .HasColumnType("decimal(18, 2)");
+        }
     }
 }
--- FinanceFlowAPI/Program.cs
+++ FinanceFlowAPI/Program.cs
@@ -29,9 +29,16 @@ builder.Services.AddSwaggerGen(option =>
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+
+
+
 builder.Services.AddAuthorization();
-builder.Services.AddIdentityApiEndpoints<IdentityUser>()
-    .AddEntityFrameworkStores<AppDbContext>();
+//builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddAuthentication(options =>
 {
